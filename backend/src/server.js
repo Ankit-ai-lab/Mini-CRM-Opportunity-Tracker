789c45340f88ba8b0b8b80186afa8ebdbfe3e0ae
Ankit-ai-lab <ankitguptame98@gmail.com>
2026-06-23 20:37:09 +0530

    backend test done

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,12 @@ const app = require('./app');
 const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
-
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Mini CRM API is running successfully"
+  });
+});
 const startServer = async () => {
   await connectDB();
   app.listen(PORT, () => {
